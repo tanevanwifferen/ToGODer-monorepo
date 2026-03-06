578159b6f991d25a31c65fc6d7a1be10ee9aedf3
Tane <tane@example.com>
2026-03-06 16:47:06 +0000

    Remove legacy Vue frontend - app is now standalone

--- packages/core/Frontend/ApiClient/GlobalApiClient.js
+++
@@ -1,9 +0,0 @@
-class GlobalApiClient {
-  static async getGlobalConfig() {
-    return ApiClient.get('/global_config');
-  }
-
-  static async getPrompts() {
-    return ApiClient.get('/prompts');
-  }
-}
